55743f8ace89849c26ac66b3ccb555ddbe022ad9
NagomiJugon <fumiya1388touhoku@gmail.com>
2022-10-07 15:05:40 +0900

    余分なimport文を削除

--- src/main/java/com/example/mail/service/AppUserServiceImpl.java
+++ src/main/java/com/example/mail/service/AppUserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.example.mail.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
